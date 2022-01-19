423d619389f19029c6032278538a8ecae1cb9f91
Bruno Paulino <brunojppb@gmail.com>
2022-01-19 15:41:21 -0300

    Add custom task for building frontend

---
+++ lib/mix/tasks/webapp.ex
@@ -0,0 +1,24 @@
+defmodule Mix.Tasks.Webapp do
+  @moduledoc """
+    React frontend compilation and bundling for production.
+  """
+  use Mix.Task
+  require Logger
+  # Path for the frontend static assets that are being served
+  # from our Phoenix router when accessing /app/* for the first time
+  @public_path "./priv/static/webapp"
+
+  @shortdoc "Compile and bundle React frontend for production"
+  def run(_) do
+    Logger.info("📦 - Installing NPM packages")
+    System.cmd("npm", ["install", "--quiet"], cd: "./frontend")
+
+    Logger.info("⚙️  - Compiling React frontend")
+    System.cmd("npm", ["run", "build"], cd: "./frontend")
+
+    Logger.info("🚛 - Moving dist folder to Phoenix at #{@public_path}")
+    System.cmd("cp", ["-R", "./frontend/dist", @public_path])
+
+    Logger.info("⚛️  - React frontend ready.")
+  end
+end
